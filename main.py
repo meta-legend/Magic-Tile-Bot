ede98c9b4488cba54ec3a032eb6e8e3d407d71a7
Pranab Shukla <pranab.austin@gmail.com>
2022-10-19 18:08:46 -0500

    This adds the python file to the repository

---
+++ main.py
@@ -0,0 +1,30 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+
+# Tile 1 Position: X:  581 Y:  400 RGB: ( 77,  80, 115)
+# Tile 2 Position: X:  682 Y:  400 RGB: (  0,   0,   0)
+# Tile 3 Position: X:  770 Y:  400 RGB: ( 79,  82, 116)
+# Tile 4 Position: X:  869 Y:  400 RGB: ( 80,  83, 116)
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(0.1)  # This pauses the script for 0.1 seconds
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+
+
+while keyboard.is_pressed('q') == False:
+
+    if pyautogui.pixel(370, 536)[0] == 0:
+        click(370, 536)
+    if pyautogui.pixel(458, 536)[0] == 0:
+        click(458, 536)
+    if pyautogui.pixel(535, 536)[0] == 0:
+        click(535, 536)
+    if pyautogui.pixel(626, 536)[0] == 0:
+        click(626, 536)
